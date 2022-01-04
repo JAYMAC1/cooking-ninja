3b71fff3fb3b7500f9fd0464f2ba6e661818f6d0
James McNeil <mcneil_james@hotmail.com>
2022-01-04 17:20:41 +0000

    added form to Create component

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,54 @@
 // Styles
+import { useState } from 'react'
 import './Create.css'
 
 const Create = () => {
-  return <div>Create</div>
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(method)
+    console.log(cookingTime)
+  }
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* ingrediants goes here */}
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking Time (mins):</span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button className='button'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Create
