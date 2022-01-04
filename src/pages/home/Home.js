9d3bf099fed9e2fe8bbc347b922369790ead8d74
James McNeil <mcneil_james@hotmail.com>
2022-01-04 13:58:05 +0000

    imported RecipeList and moved fetched data to this

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,8 @@ import { useFetch } from '../../hooks/useFetch'
 // styles
 import './Home.css'
 
+// Components
+import RecipeList from '../../components/RecipeList'
 const Home = () => {
   const url = 'http://localhost:3000/recipes'
   const { data, isPending, error } = useFetch(url)
@@ -10,7 +12,7 @@ const Home = () => {
     <div className='home'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
-      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
