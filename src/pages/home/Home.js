3702e3147c9547ce0caad68964f9155b5b940560
James McNeil <mcneil_james@hotmail.com>
2022-01-04 13:43:13 +0000

    pulled data with useFetch and basic output to the Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,18 @@
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Home.css'
 
 const Home = () => {
-  return <div>Home</div>
+  const url = 'http://localhost:3000/recipes'
+  const { data, isPending, error } = useFetch(url)
+  return (
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  )
 }
 
 export default Home
