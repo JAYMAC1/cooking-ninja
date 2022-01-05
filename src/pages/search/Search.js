abba3164327151648c32dab94568211752284b47
James McNeil <mcneil_james@hotmail.com>
2022-01-05 19:57:31 +0000

    created output fr searched items pulling data from url queryString

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,8 +1,27 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 // Styles
 import './Search.css'
 
+import RecipeList from '../../components/RecipeList'
+
 const Search = () => {
-  return <div>Search</div>
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  const url = `http://localhost:3000/recipes?q=${query}`
+
+  const { data, isPending, error } = useFetch(url)
+
+  return (
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  )
 }
 
 export default Search
