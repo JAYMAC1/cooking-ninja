6c97163d779225d0ba8d74f910dfcf7f54d68c5e
James McNeil <mcneil_james@hotmail.com>
2022-01-05 19:04:51 +0000

    added ingredient input and redirect when submitted

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,16 +1,46 @@
 // Styles
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { Redirect, useHistory } from 'react-router-dom'
 import './Create.css'
+import { useFetch } from '../../hooks/useFetch'
 
 const Create = () => {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientsInput = useRef(null)
+
+  const { postData, data, error } = useFetch(
+    'http://localhost:3000/recipes',
+    'POST'
+  )
+  const history = useHistory()
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title)
-    console.log(method)
-    console.log(cookingTime)
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + ' minutes',
+    })
+  }
+
+  useEffect(() => {
+    if (data) {
+      history.push('/')
+    }
+  }, [data, history])
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIng) => [...prevIng, ing])
+    }
+    setNewIngredient('')
+    ingredientsInput.current.focus()
   }
   return (
     <div className='create'>
@@ -25,8 +55,26 @@ const Create = () => {
             required
           />
         </label>
-
-        {/* ingrediants goes here */}
+        <label>
+          <span>Recipe ingredients</span>
+          <div className='ingredients'>
+            <input
+              type='text'
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientsInput}
+            />
+            <button onClick={handleAdd} className='btn'>
+              Add
+            </button>
+          </div>
+        </label>
+        <p className='ingredients'>
+          Current ingredients:{' '}
+          {ingredients.map((ing) => (
+            <em key={ing}>{ing}, </em>
+          ))}
+        </p>
 
         <label>
           <span>Recipe method:</span>
